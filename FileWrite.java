26fd4a22297c415b5fa862edac3e444baa101e1e
Chase <102624440+uChase@users.noreply.github.com>
2023-08-29 11:52:55 -0700

    string fix

--- FileWrite.java
+++ FileWrite.java
@@ -10,7 +10,8 @@ public class FileWrite {
     // test
     public static void main(String[] args) {
         FileWrite.writeToFile("hello \n goodbye", "test.txt");
-        readFile("test.txt");
+        String str = readFile("test.txt");
+        System.out.println(str);
     }
 
     static void writeToFile(String text, String fileName) {
@@ -27,27 +28,35 @@ public class FileWrite {
 
     }
 
-    static void readFile(String file) {
+    static String readFile(String file) {
         FileReader fr;
         try {
             fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             String c;
+            String sub = "";
             try {
                 while (br.ready()) {
                     c = br.readLine();
                     System.out.println(c);
+                    sub += c;
+                    if (br.ready()) {
+                        sub += "\n";
+                    }
                 }
                 br.close();
                 fr.close();
+                return sub;
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
 
+            return null;
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        return null;
     }
 }
